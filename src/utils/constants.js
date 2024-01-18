d9be2e18c379082583d8267534a971ca91de7b96
akshay tuptewar <81164399+akshaytuptewar19@users.noreply.github.com>
2024-01-18 12:44:03 +0530

    Add files via upload

---
+++ src/utils/constants.js
@@ -0,0 +1,9 @@
+import abi from "./CryptoWill.json";
+import landAbi from "./LandWill.json";
+
+export const contractABI = abi;
+export const contractAddress = "0xf129D79FD8737725e2c83aC4aBdC29B0f86D82F9";
+
+export const landContractABI = landAbi;
+export const landContractAddress = "0xF0fac94E862E14aAD7A327071bcD81Fb41F98A0D";
+
